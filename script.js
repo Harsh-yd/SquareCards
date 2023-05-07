697c3711352709e483237542a095e532f9ef6371
Harsh-yd <132829664+Harsh-yd@users.noreply.github.com>
2023-05-07 19:30:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+document.getElementsByClassName("fa-brands fa-youtube")[0].addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=aYGY-oklFGU&list=LL&index=3");
+}
+)
+
+document.getElementsByClassName("fa-brands fa-youtube")[1].addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=aYGY-oklFGU&list=LL&index=3");
+}
+)
+
+document.getElementsByClassName("fa-brands fa-youtube")[2].addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=aYGY-oklFGU&list=LL&index=3");
+}
+)
+
+document.getElementsByClassName("article")[0].addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=aYGY-oklFGU&list=LL&index=3");
+}
+)
+
+document.getElementsByClassName("article")[1].addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=aYGY-oklFGU&list=LL&index=3");
+}
+)
+
+document.getElementsByClassName("article")[2].addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=aYGY-oklFGU&list=LL&index=3");
+}
+)
+
